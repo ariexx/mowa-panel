dc6e26c838142df0f5cefd43489aba5e550b7f99
Arief <mgi.official1@gmail.com>
2024-10-26 08:55:00 +0700

    refactor: using .env

--- src/service/AdminService.js
+++ src/service/AdminService.js
@@ -1,28 +1,24 @@
+import axios from 'axios';
+
 export const AdminService = {
     async getStatisticsData() {
         try {
-            const response = await fetch('https://f4db56d0-a569-414e-b0e5-19a31b12e53b.mock.pstmn.io/api/v1/admin/dashboard-statistic');
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            const data = await response.json();
-            return data;
+            const response = await axios.get(`${process.env.VUE_APP_BACKEND_URL}/api/v1/admin/dashboard-statistic`);
+            return response.data;
         } catch (error) {
-            console.error('There was a problem with the fetch operation:', error);
+            console.error('There was a problem with the axios operation:', error);
             throw error;
         }
     },
 
     async getLastOrders({ limit }) {
         try {
-            const response = await fetch('https://f4db56d0-a569-414e-b0e5-19a31b12e53b.mock.pstmn.io/api/v1/admin/last-orders?limit=' + limit);
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            const data = await response.json();
-            return data;
+            const response = await axios.get(`${process.env.VUE_APP_BACKEND_URL}/api/v1/admin/last-orders`, {
+                params: { limit }
+            });
+            return response.data;
         } catch (error) {
-            console.error('There was a problem with the fetch operation:', error);
+            console.error('There was a problem with the axios operation:', error);
             throw error;
         }
     }
